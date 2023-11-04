f02e9bcf14a3ebae8ccd4b709ffe644578f0f786
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2023-11-04 18:10:41 +0000

    Added sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,14 +7,15 @@ import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { MobileSidebar } from "@/components/mobile-sidebar";
 
 const font = Poppins({ weight: "600", subsets: ["latin"] });
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <MobileSidebar/>
         <Link href="/">
           <h1
             className={cn(
