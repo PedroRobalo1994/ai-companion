39bb78d262f8e7c7aef02e78898f0417051afce6
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2023-11-09 01:44:07 +0000

    Added chat header feature

---
+++ app/(chat)/layout.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+type ChatLayoutProps = {
+  children: React.ReactNode;
+};
+
+const ChatLayout = ({ children }: {children: React.ReactNode}) => {
+  return (
+    <div className="mx-auto max-w-4xl  h-full w-full">
+        {children}
+    </div>
+  );
+};
+
+export default ChatLayout;
--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,8 +1,8 @@
 import prismadb from "@/lib/prismadb"
-import { currentUser } from "@clerk/nextjs"
+import { auth, currentUser } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 
-export async function PATCH(req:Request, { params }: { params: { companionId: string } }){
+export async function PATCH(req :Request, { params }: { params: { companionId: string } }){
     try {
         const body = await req.json()
         const user = await currentUser()
@@ -39,6 +39,29 @@ export async function PATCH(req:Request, { params }: { params: { companionId: st
 
     } catch (error) {
         console.log("[COMPANION_PATCH]", error)
+        return new NextResponse("Internal Error", { status: 500 })
+    }
+}
+
+export async function DELETE(req: Request, { params }: { params: { companionId: string } }){
+    try {
+        const {userId} = auth()
+
+        if(!userId){
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+
+        const companion = await prismadb.companion.delete({
+            where: {
+                userId,
+                id: params.companionId
+            }
+        })
+
+        return NextResponse.json(companion)
+
+    } catch (error) {
+        console.log("[COMPANION_DELETE]", error)
         return new NextResponse("Internal Error", { status: 500 })
     }
 }
