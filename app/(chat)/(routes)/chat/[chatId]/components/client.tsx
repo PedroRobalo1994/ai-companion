952c48d33fe74160430968fd543a0b02bc158da0
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2023-11-12 19:51:35 +0000

    Added memory service and chat api integration

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,55 +1,76 @@
-'use client'
+"use client";
 
-import { ChatHeader } from "@/components/chat-header";
-import { Companion, Message } from "@prisma/client"
-import { useRouter } from "next/navigation";
+import { useCompletion } from "ai/react";
 import { FormEvent, useState } from "react";
-import { useCompletion} from 'ai/react' 
+import { Companion, Message } from "@prisma/client";
+import { useRouter } from "next/navigation";
+
 import { ChatForm } from "@/components/chat-form";
-import {ChatMessages} from "@/components/chat-messages";
+import { ChatHeader } from "@/components/chat-header";
+import { ChatMessages } from "@/components/chat-messages";
 import { ChatMessageProps } from "@/components/chat-message";
 
 interface ChatClientProps {
-    companion: Companion & {
-        messages: Message[];
-        _count: {
-            messages: number;
-        }
+  companion: Companion & {
+    messages: Message[];
+    _count: {
+      messages: number;
     }
-}
+  };
+};
 
-export const ChatClient = ({companion}: ChatClientProps) => {
-    const router = useRouter()
-    const [messages, setMessages] = useState<ChatMessageProps[]>(companion.messages)
-    const {input, isLoading, handleInputChange, handleSubmit, setInput} = useCompletion({
-        api: `/api/${companion.id}`,
-        onFinish(prompt, completion){
-            const systemMessage: ChatMessageProps = {
-                role: 'system',
-                content: completion
-            }
-            setMessages((current: any) => [...current, systemMessage])
-            setInput("")
-            router.refresh()
-        }})
+export const ChatClient = ({
+  companion,
+}: ChatClientProps) => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatMessageProps[]>(companion.messages);
+  
+  const {
+    input,
+    isLoading,
+    handleInputChange,
+    handleSubmit,
+    setInput,
+  } = useCompletion({
+    api: `/api/chat/${companion.id}`,
+    onFinish(_prompt, completion) {
+      const systemMessage: ChatMessageProps = {
+        role: "system",
+        content: completion
+      };
 
-    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-        const userMessage: ChatMessageProps = {
-            role: 'user',
-            content: input
-        }
-        setMessages((current: any) => [...current, userMessage])
-        handleSubmit(e)
-    }
-    return (
-        <div className="flex flex-col h-full p-4 space-y-2">
-            <ChatHeader companion={companion} />
-            <ChatMessages companion={companion} isLoading={isLoading} messages={messages}/>
-            <ChatForm 
-            isLoading={isLoading} 
-            input={input} 
-            handleInputChange={handleInputChange} 
-            onSubmit={onSubmit}/>
-        </div>
-    )
+      setMessages((current) => [...current, systemMessage]);
+      setInput("");
+
+      router.refresh();
+    },
+  });
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const userMessage: ChatMessageProps = {
+      role: "user",
+      content: input
+    };
+
+    setMessages((current) => [...current, userMessage]);
+
+    handleSubmit(e);
+  }
+
+  return (
+    <div className="flex flex-col h-full p-4 space-y-2">
+      <ChatHeader companion={companion} />
+      <ChatMessages 
+        companion={companion}
+        isLoading={isLoading}
+        messages={messages}
+      />
+      <ChatForm 
+        isLoading={isLoading} 
+        input={input} 
+        handleInputChange={handleInputChange} 
+        onSubmit={onSubmit} 
+      />
+    </div>
+   );
 }
--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -25,6 +25,6 @@ export const ChatMessages = ({messages = [], isLoading, companion}: ChatMessages
         scrollRef?.current?.scrollIntoView({behavior: 'smooth'})
     }, [messages.length])
     return ( <div className="flex-1 overflow-y-auto pr-4"><ChatMessage isLoading={fakeLoading} src={companion.src} role="system" content={`Hello, I am ${companion.name}, ${companion.description}`}/>
-    {messages.map((message) => (<ChatMessage key={message.content} role={message.role} content={message.content} src={message.src}/>))} {isLoading && (<ChatMessage role="system" src={companion.src} isLoading/>)}
+    {messages.map((message) => (<ChatMessage key={message.content} role={message.role} content={message.content} src={companion.src}/>))} {isLoading && (<ChatMessage role="system" src={companion.src} isLoading/>)}
     <div ref={scrollRef} /></div> );
 }
