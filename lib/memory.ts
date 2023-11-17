c275b7765f449b716e570a37e4c44e236828647f
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2023-11-17 19:37:07 +0000

    Fix for deploy

--- lib/memory.ts
+++ lib/memory.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { Pinecone } from "@pinecone-database/pinecone";
+import { PineconeClient } from "@pinecone-database/pinecone";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 
 export type CompanionKey = {
@@ -12,15 +12,15 @@ export type CompanionKey = {
 export class MemoryManager {
   private static instance: MemoryManager;
   private history: Redis;
-  private vectorDBClient: Pinecone;
+  private vectorDBClient: PineconeClient;
 
   public constructor() {
     this.history = Redis.fromEnv();
-    this.vectorDBClient = new Pinecone();
+    this.vectorDBClient = new PineconeClient();
   }
 
   public async init() {
-    if (this.vectorDBClient instanceof Pinecone) {
+    if (this.vectorDBClient instanceof PineconeClient) {
       await this.vectorDBClient.init({
         apiKey: process.env.PINECONE_API_KEY!,
         environment: process.env.PINECONE_ENVIRONMENT!,
@@ -32,7 +32,7 @@ export class MemoryManager {
     recentChatHistory: string,
     companionFileName: string
   ) {
-    const pineconeClient = <Pinecone>this.vectorDBClient;
+    const pineconeClient = <PineconeClient>this.vectorDBClient;
 
     const pineconeIndex = pineconeClient.Index(
       process.env.PINECONE_INDEX! || ""
