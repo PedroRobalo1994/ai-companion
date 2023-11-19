10f38b5db09d5ac882dcc90f76bff8bd3d53ede4
Pedro Robalo <35338193+PedroRobalo1994@users.noreply.github.com>
2023-11-19 15:01:41 +0000

    Add favicon

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,31 +1,42 @@
+import { redirect } from "next/navigation";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 import prismadb from "@/lib/prismadb";
+import { checkSubscription } from "@/lib/subscription";
 import { CompanionForm } from "./components/companion-form";
-import { auth, redirectToSignIn} from "@clerk/nextjs";
 
 interface CompanionIdPageProps {
   params: {
     companionId: string;
   };
-}
+};
 
-const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
-  //Todo: Check subscription
-
-  const {userId} = auth();
+const CompanionIdPage = async ({
+  params
+}: CompanionIdPageProps) => {
+  const { userId } = auth();
 
   if (!userId) {
     return redirectToSignIn();
   }
 
+  const validSubscription = await checkSubscription();
+
+  if (!validSubscription) {
+    return redirect("/");
+  }
+
   const companion = await prismadb.companion.findUnique({
-    where: { 
+    where: {
+      id: params.companionId,
       userId,
-      id: params.companionId },
+    }
   });
 
   const categories = await prismadb.category.findMany();
 
-  return <CompanionForm initialData={companion} categories={categories} />;
-};
-
+  return ( 
+    <CompanionForm initialData={companion} categories={categories} />
+  );
+}
+ 
 export default CompanionIdPage;
